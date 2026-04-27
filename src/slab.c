bc4eff7667d6c1c7cb0d09dfa8a0c2d6e7f8aae9
Koshtak <lorerime101@gmail.com>
2026-04-27 20:02:28 +0300

    alloc mostly done

--- src/slab.c
+++ src/slab.c
@@ -6,36 +6,54 @@
 
 
 SlabPool* slab_init(size_t obj_size, size_t count){
-size_t bitmap_bytes = (count+7)/8;
-size_t total_size =sizeof(SlabPool)+ bitmap_bytes + (count*obj_size);
+	size_t bitmap_bytes = (count+7)/8;
+	size_t total_size =sizeof(SlabPool)+ bitmap_bytes + (count*obj_size);
 
-void *mem = mmap(NULL, total_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, MAP_ANONYMOUS, -1, 0);
+	void *mem = mmap(NULL, total_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, MAP_ANONYMOUS, -1, 0);
 
-if(mem==MAP_FAILED){
-	perror("mmap failed");
-	return NULL;}
+	if(mem==MAP_FAILED){
+		perror("mmap failed");
+		return NULL;}
 
-SlabPool *pool = (SlabPool*)mem;
+	SlabPool *pool = (SlabPool*)mem;
 
 
-pool->obj_size = obj_size;
-pool->obj_size = obj_size;
-pool->total_objs = count;
-pool->start =(uint8_t*)(pool->bitmap + bitmap_bytes);
+	pool->obj_size = obj_size;
+	pool->obj_size = obj_size;
+	pool->total_objs = count;
+	pool->start =(uint8_t*)(pool->bitmap + bitmap_bytes);
 
-memset(pool->bitmap, 0, pool->bitmap_bytes);
+	memset(pool->bitmap, 0, pool->bitmap_bytes);
 
 return pool;
 }
 
 
 
-/*
+
 void* slab_alloc(SlabPool *pool){
-	for(size_t i=0;i<=pool->total_objs/8;i++){
+	size_t bitmap_bytes = (pool->total_objs+7)/8;
+	uint8_t *bitmap=pool->bitmap;
+
+	for(size_t i=0;i<=bitmap_bytes;i++){
 		if(pool->bitmap[i]==0xFF){i++}
-		else for(int j;j<8;j++)
-	
+		else for(int bit;bit<8;bit++)
+		{
+			size_t bm_location =(i*8)+bit;
+			/*
+			if(bm_location >= pool->total_objs)break;
+			
+			if(!(bitmap[i]&(1<<bit)) )
+				{bitmap[i] |= (1<<bit);
+			return(uint8_t*)pool->start+(bm_location*pool->obj_size);
+			}
+			
+			
+			
+			*/
+		}
+
+
 	}	
 	
 }
@@ -43,7 +61,7 @@ void* slab_alloc(SlabPool *pool){
 
 
 
-
+/*
 void slab_free(SlabPool *pool, void *ptr){
 
 
